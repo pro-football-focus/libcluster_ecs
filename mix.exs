3a8005d3ef5f8988ac4fe782687c9f012ca5a42a
Geoff Lane <geoff@zorched.net>
2018-09-10 12:59:50 -0400

    Bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClusterEcs.MixProject do
   def project do
     [
       app: :libcluster_ecs,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       name: "libcluster_ecs",
       source_url: "https://github.com/pro-football-focus/libcluster_ecs",
